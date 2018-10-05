22d95365634ed74391eea94d0d44ba1f9400550c
Nate Miller <natekmiller96@gmail.com>
2018-10-05 17:15:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function addNewLiOnClick(){
 
 function clearEmployeeListOnLinkClick(){
   document.querySelector("a").addEventListener("click", function(event){
-    document.querySelector("ul").value = "";
+    document.querySelector("ul").InnerHTML = "";
   });
 }
