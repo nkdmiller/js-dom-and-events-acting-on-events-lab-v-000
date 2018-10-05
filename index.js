b44b386b59da1e03093df2ca635cde205b8a2a52
Nate Miller <natekmiller96@gmail.com>
2018-10-05 17:16:23 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation(){
+  return document.querySelector("input[name='name']").value;
+}
+
+function addNewElementAsLi(){
+  return document.querySelector("ul.employee-list").append(retrieveEmployeeInformation());
+}
 preventRefreshOnSubmit()
+
+function addNewLiOnClick(){
+  document.querySelector("input[type='submit']").addEventListener("click", function(event){
+    addNewElementAsLi();
+    document.querySelector("input[name='name']").value = "";
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  document.querySelector("a").addEventListener("click", function(event){
+    document.querySelector("ul").innerHTML = "";
+  });
+}
